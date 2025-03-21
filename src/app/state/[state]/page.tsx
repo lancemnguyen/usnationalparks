f86139764654982562865e44abf10125a156ac76
lance nguyen <lancesudo@gmail.com>
2025-03-20 20:46:07 -0700

    remove import

--- src/app/state/[state]/page.tsx
+++ src/app/state/[state]/page.tsx
@@ -1,4 +1,4 @@
-import { Metadata } from 'next';
+// import { Metadata } from 'next';
 // import { notFound } from 'next/navigation';
 import { alaskaParks } from '@/data/alaska-parks';
 import { arizonaParks } from '@/data/arizona-parks';
